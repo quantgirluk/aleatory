7aecc981c27baf463a94b24976a71452d3f8be15
Quant Girl <quantgirl.uk@gmail.com>
2022-11-03 22:17:25 +0000

    Refactoring

--- replica/utils.py
+++ replica/utils.py
@@ -2,7 +2,6 @@ from numbers import Number
 import numpy as np
 
 
-
 def get_times(end, n):
     """Generate a linspace from 0 to end for n increments."""
     return np.linspace(0, end, n + 1)
@@ -27,3 +26,17 @@ def check_positive_number(value, name=""):
     check_numeric(value, name)
     if value <= 0:
         raise ValueError(f"{name} value must be positive.")
+
+
+def check_increments(times):
+    increments = np.diff(times)
+    if np.any([t < 0 for t in times]):
+        raise ValueError("Times must be nonnegative.")
+    if np.any([t <= 0 for t in increments]):
+        raise ValueError("Times must be strictly increasing.")
+    return increments
+
+
+def times_to_increments(times):
+    """Ensure a positive, monotonically increasing sequence."""
+    return check_increments(times)
