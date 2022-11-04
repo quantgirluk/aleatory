e883bdbd13b1e1d7ad7af30cc55ff644ef2b9ca0
Quant Girl <quantgirl.uk@gmail.com>
2022-11-04 23:25:37 +0000

    Update testing.py

--- replica/testing.py
+++ replica/testing.py
@@ -3,5 +3,10 @@ import matplotlib.pyplot as plt
 
 my_process = Gaussian()
 n =200
+
+print(my_process.sample_at(times=[1,2,3, 4.5]))
+
+
 plt.plot(my_process.times(n=n), my_process.sample(n=n))
 plt.show()
+
