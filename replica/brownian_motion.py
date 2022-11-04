1f02872445d415224ae8d17a95d4b80155e2d2ab
Quant Girl <quantgirl.uk@gmail.com>
2022-11-04 23:25:35 +0000

    Create brownian_motion.py

---
+++ replica/brownian_motion.py
@@ -0,0 +1,43 @@
+from gaussian import Gaussian
+from utils import check_positive_number, check_numeric, get_times
+import numpy as np
+
+class BrownianMotion(Gaussian):
+
+    def __init__(self, drift=0.0, scale=1.0, T=1.0, rng=None):
+        super().__init__(T=T, rng=rng)
+        self.drift = drift
+        self.scale = scale
+        self.times = None
+
+    @property
+    def drift(self):
+        return self._drift
+
+    @drift.setter
+    def drift(self, value):
+        check_numeric(value, "Drift")
+        self._drift = value
+
+    @property
+    def scale(self):
+        return self._scale
+
+    @scale.setter
+    def scale(self, value):
+        check_positive_number(value, "Scale")
+        self._scale = value
+
+    def _sample_brownian_motion(self, n):
+
+        if self.drift !=0 and (self._times is None or len(self._times)!= n):
+            self._n = n
+            self._times = get_times(self.drift, n)
+
+        bm = np.cumsum(self.scale * self._sample_gaussian_noise(n))
+        bm = np.insert(bm, [0], 0)
+
+        if self.drift == 0:
+            return bm
+        else:
+            return self._times + bm
