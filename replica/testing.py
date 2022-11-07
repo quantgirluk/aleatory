75c43596328d16bd90e0170add50bcbc9a23d11d
Quant Girl <quantgirl.uk@gmail.com>
2022-11-07 20:33:58 +0000

    Update testing.py

--- replica/testing.py
+++ replica/testing.py
@@ -1,12 +1,21 @@
 from gaussian import Gaussian
+from brownian_motion import BrownianMotion
 import matplotlib.pyplot as plt
 
 my_process = Gaussian()
 n =200
 
 print(my_process.sample_at(times=[1,2,3, 4.5]))
-
-
 plt.plot(my_process.times(n=n), my_process.sample(n=n))
 plt.show()
 
+
+n = 200
+my_brownian = BrownianMotion()
+sample = my_brownian.sample(n=n)
+times = my_brownian._times
+# print(times)
+# print(sample)
+
+plt.plot(times, sample)
+plt.show()
