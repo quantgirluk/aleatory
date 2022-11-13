a9f49d582066406b102e01b2725e9ef6b03662c0
Quant Girl <quantgirl.uk@gmail.com>
2022-11-13 17:49:01 +0000

    Update testing.py

--- replica/testing.py
+++ replica/testing.py
@@ -1,8 +1,11 @@
+import unittest
 import numpy as np
-
+from brownian_motion_paths import BrownianPaths
+from geometric_brownian_motion import GeometricBrownianMotion
 from gaussian import Gaussian
 from brownian_motion import BrownianMotion
 import matplotlib.pyplot as plt
+
 
 # my_process = Gaussian()
 # n =200
@@ -20,19 +23,35 @@ import matplotlib.pyplot as plt
 #     plt.plot(times, sample)
 # plt.show()
 
-# my_times = np.linspace(0, 5, 100, endpoint=True)
-# for k in range(10):
-#     my_brownian = BrownianMotion(drift=-1.0, scale=2.0)
-#     sample = my_brownian.sample_at(my_times)
-#     plt.plot(my_times, sample)
-# plt.show()
+class TestProcesses(unittest.TestCase):
 
-from brownian_motion_paths import BrownianPaths
+    def test_BrownianMotion(self):
+        my_times = np.linspace(0, 5, 100, endpoint=True)
+        for k in range(10):
+            my_brownian = BrownianMotion(drift=-1.0, scale=2.0)
+            sample = my_brownian.sample_at(my_times)
+            plt.plot(my_times, sample)
+        plt.show()
+        self.assertEqual(len(my_times), len(my_brownian.times))
+        for t, s in zip(my_times, my_brownian.times):
+            self.assertEqual(t, s)
 
-my_times = np.linspace(0, 1, 100, endpoint=True)
-BMP = BrownianPaths(N=200, times=my_times, drift=1.0, scale=1.5)
+    def test_GeometricBrownianMotion(self):
+        my_times = np.linspace(0, 1, 100, endpoint=True)
+        for k in range(20):
+            my_process = GeometricBrownianMotion(drift=2.0, volatility=0.5)
+            sample = my_process.sample_at(my_times)
+            plt.plot(my_times, sample)
+        plt.show()
 
 
-BMP.draw()
+class TestPaths(unittest.TestCase):
+    def test_BrownianPaths(self):
+        my_times = np.linspace(0, 1, 100, endpoint=True)
+        BMP = BrownianPaths(N=200, times=my_times, drift=1.0, scale=1.5)
+        BMP.plot()
+        BMP.draw()
 
 
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
