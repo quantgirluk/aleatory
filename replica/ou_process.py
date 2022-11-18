7a607804181d91d179f9555fbd20dc0dbe987101
Quant Girl <quantgirl.uk@gmail.com>
2022-11-18 23:11:37 +0000

    Fixed g coefficient in OU process

--- replica/ou_process.py
+++ replica/ou_process.py
@@ -1,6 +1,7 @@
 from base_EMProcess import BaseEulerMaruyamaProcess
 import numpy as np
 
+
 class OUProcess(BaseEulerMaruyamaProcess):
 
     def __init__(self, theta=1.0, mu=1.0, sigma=1.0, initial=0.0, T=1.0, n=1, rng=None):
@@ -12,11 +13,12 @@ class OUProcess(BaseEulerMaruyamaProcess):
         self.n = n
         self.dt = 1.0 * self.T / self.n
         self.times = np.arange(0.0, self.T + self.dt, self.dt)
+
         def f(x, _):
             return self.theta * (self.mu - x)
 
         def g(x, _):
-            return self.sigma * x
+            return self.sigma
 
         self.f = f
         self.g = g
