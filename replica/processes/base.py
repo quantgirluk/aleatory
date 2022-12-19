Fix EM Scheme to Avoid Overflows. Added log option to simulate processes which can have numerical issues due to negative numbers.

replica/processes/base.py
@@ -166,27 +166,35 @@ class SPExplicit(StochasticProcess):
 
 class SPEulerMaruyama(SPExplicit):
     def __int__(self, f=None, g=None, initial=0.0, T=1.0, rng=None):
-        super().__init__(T=T, rng=rng)
+        super().__init__(T=T, rng=rng, initial=initial)
         self.f = f
         self.g = g
-        self.initial = initial
         self.n = None
         self.dt = None
         self.times = None
         self.name = None
 
-    def _sample_em_process(self, n):
+    def _sample_em_process(self, n, log=False):
         check_positive_integer(n)
         self.n = n
         self.dt = 1.0 * self.T / self.n
         self.times = get_times(self.T, self.n)
         dws = self.rng.normal(scale=np.sqrt(self.dt), size=self.n)
-        path = [self.initial]
-        previous = self.initial
+
+        if log:
+            origin = np.log(self.initial)
+        else:
+            origin = self.initial
+
+        path = [origin]
+        previous = origin
 
         for (t, dw) in zip(self.times[1:], dws[1:]):
             previous += self.f(previous, t) * self.dt + self.g(previous, t) * dw
             path.append(previous)
+
+        if log:
+            return np.exp(path)
 
         return path
 

replica/processes/euler_maruyama/cev_process.py
@@ -51,10 +51,12 @@ class CEVProcess(SPEulerMaruyama):
         self._marginals = None
 
         def f(x, _):
-            return self.mu * x
+            return self.mu - 0.5 * (self.sigma ** 2) * np.exp(2.0 * (self.gamma - 1.0) * x)
+            # return self.mu * x
 
         def g(x, _):
-            return self.sigma * (x ** self.gamma)
+            return self.sigma * np.exp((self.gamma - 1.0) * x)
+            # return self.sigma * (x ** self.gamma)
 
         self.f = f
         self.g = g
@@ -200,3 +202,6 @@ class CEVProcess(SPEulerMaruyama):
         else:
             fig = self._draw_paths_kde(expectations=expectations, **fig_kw)
         return fig
+
+    def sample(self, n):
+        return self._sample_em_process(n, log=True)

replica/processes/euler_maruyama/cir_process.py
@@ -36,22 +36,24 @@ class CIRProcess(SPEulerMaruyama):
     :param numpy.random.Generator rng: a custom random number generator
 
     """
-    def __init__(self, theta=1.0, mu=1.0, sigma=1.0, initial=0.0, T=1.0, rng=None):
-        super().__init__(T=T, rng=rng)
+
+    def __init__(self, theta=1.0, mu=1.0, sigma=1.0, initial=1.0, T=1.0, rng=None):
+        super().__init__(T=T, rng=rng, initial=initial)
         self.theta = theta
         self.mu = mu
         self.sigma = sigma
-        self.initial = initial
         self.n = 1.0
         self.dt = 1.0 * self.T / self.n
         self.times = np.arange(0.0, self.T + self.dt, self.dt)
         self.name = "CIR Process"
 
         def f(x, _):
-            return self.theta * (self.mu - x)
+            # return self.theta * (self.mu - x)
+            return np.exp(-x) * (self.theta * (self.mu - np.exp(x)) - 0.5 * self.sigma ** 2)
 
         def g(x, _):
-            return self.sigma * np.sqrt(x)
+            # return self.sigma * np.sqrt(x)
+            return self.sigma * np.exp(-0.5 * x)
 
         self.f = f
         self.g = g
@@ -112,9 +114,20 @@ class CIRProcess(SPEulerMaruyama):
 
     def get_marginal(self, t):
         nu = 4.0 * self.theta * self.mu / self.sigma ** 2
-        ct = 4.0 * self.theta / ((self.sigma ** 2) * (1.0 - np.exp(-4.0 * self.theta)))
+        ct = 4.0 * self.theta / ((self.sigma ** 2) * (1.0 - np.exp(-1.0 * self.theta)))
         lambda_t = ct * self.initial * np.exp(-1.0 * self.theta * t)
         scale = 1.0 / (4.0 * self.theta / ((self.sigma ** 2) * (1.0 - np.exp(-1.0 * self.theta * t))))
         marginal = ncx2(nu, lambda_t, scale=scale)
+
+        # c = 2 * self.theta / ((1.0 - np.exp(-1.0*self.theta * t)) * self.sigma ** 2)
+        # q = 2 * self.theta * self.mu / self.sigma ** 2 - 1
+        # u = c * self.initial * np.exp(-self.theta * t)
+        # df = 2 * q + 2  # parameter K
+        # nc = u * 2  # parameter lambda
+        # scale = 1 / (2 * c)
+        # marginal = ncx2(df, nc, scale=scale)
+
         return marginal
 
+    def sample(self, n):
+        return self._sample_em_process(n, log=True)