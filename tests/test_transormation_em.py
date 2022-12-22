474e7bc7345ede1837745475bbcb523e4c197511
Quant Girl <quantgirl.uk@gmail.com>
2022-12-22 21:35:21 +0000

    Fix EM Scheme to Avoid Overflows. Added log option to simulate processes which can have numerical issues due to negative numbers.

--- tests/test_transormation_em.py
+++ tests/test_transormation_em.py
@@ -1,39 +1,21 @@
-from replica.processes import BrownianMotion, GBM, Vasicek, OUProcess, CIRProcess, CEVProcess
+from replica.processes import CIRProcess, CEVProcess
 
+pitaya = 'https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle'
 
 cir = CIRProcess(theta=1.0, mu=2.0, sigma=0.25)
 cev = CEVProcess(gamma=1.5, mu=0.50, sigma=0.1, initial=1.0)
 
-
-pitaya = 'https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle'
-process.draw(n=100, N=100, dpi=200)
-process.draw(n=100, N=100, style=pitaya, colormap="PiYG", dpi=200)
-process.draw(n=100, N=100,  dpi=200)
-process.draw(n=100, N=100,  colormap="Spectral", dpi=200)
-process.draw(n=100, N=100, style='Solarize_Light2', dpi=200)
-process.plot(n=100, N=100,  style=pitaya)
-process.draw(n=100, N=100, style='Solarize_Light2', colormap="coolwarm", dpi=200)
-process.draw(n=100, N=100, style='ggplot', dpi=200)
-process.draw(n=100, N=100, style='ggplot', colormap="Spectral", dpi=200)
-process.draw(n=100, N=100, style='ggplot', colormap="BrBG", dpi=200)
-process.draw(n=100, N=100, style='bmh', dpi=200)
-process.draw(n=100, N=100, style='dark_background', dpi=200)
-process.plot(n=100, N=10, figsize=(5, 3))
-
-
-
-# figure = process.plot(n=100, N=10)
-name = process.name.replace(" ", "_").lower()
-# figure.savefig(name + '_quickstart_01.png', dpi=300)
-
-# figure = process.draw(n=100, N=200)
-# figure.savefig(name + '_quickstart_02.png', dpi=300)
-#
-# figure = process.draw(n=100, N=200, envelope=True)
-# figure.savefig(name + '_quickstart_03.png', dpi=300)
-#
-# figure = process.draw(n=100, N=200, marginal=False)
-# figure.savefig(name + '_quickstart_04.png', dpi=300)
-#
-# figure = process.draw(n=100, N=200, marginal=False, envelope=True)
-# figure.savefig(name + '_quickstart_05.png', dpi=300)
+for process in [cir, cev]:
+    process.draw(n=100, N=100, dpi=200)
+    process.draw(n=100, N=100, style=pitaya, colormap="PiYG", dpi=200)
+    process.draw(n=100, N=100, dpi=200)
+    process.draw(n=100, N=100, colormap="Spectral", dpi=200)
+    process.draw(n=100, N=100, style='Solarize_Light2', dpi=200)
+    process.plot(n=100, N=100, style=pitaya)
+    process.draw(n=100, N=100, style='Solarize_Light2', colormap="coolwarm", dpi=200)
+    process.draw(n=100, N=100, style='ggplot', dpi=200)
+    process.draw(n=100, N=100, style='ggplot', colormap="Spectral", dpi=200)
+    process.draw(n=100, N=100, style='ggplot', colormap="BrBG", dpi=200)
+    process.draw(n=100, N=100, style='bmh', dpi=200)
+    process.draw(n=100, N=100, style='dark_background', dpi=200)
+    process.plot(n=100, N=10)
