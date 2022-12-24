30651fad17f3e32d7a28b056e8138d6e35b921b3
Quant Girl <quantgirl.uk@gmail.com>
2022-12-24 23:34:03 +0000

    Update test_transormation_em.py

--- tests/test_transormation_em.py
+++ tests/test_transormation_em.py
@@ -1,21 +1,7 @@
 from aleatory.processes import CIRProcess, CEVProcess
+def test_transformation_em():
+    cir = CIRProcess(theta=1.0, mu=2.0, sigma=0.25)
+    cev = CEVProcess(gamma=1.5, mu=0.50, sigma=0.1, initial=1.0)
 
-pitaya = 'https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle'
-
-cir = CIRProcess(theta=1.0, mu=2.0, sigma=0.25)
-cev = CEVProcess(gamma=1.5, mu=0.50, sigma=0.1, initial=1.0)
-
-for process in [cir, cev]:
-    process.draw(n=100, N=100, dpi=200)
-    process.draw(n=100, N=100, style=pitaya, colormap="PiYG", dpi=200)
-    process.draw(n=100, N=100, dpi=200)
-    process.draw(n=100, N=100, colormap="Spectral", dpi=200)
-    process.draw(n=100, N=100, style='Solarize_Light2', dpi=200)
-    process.plot(n=100, N=100, style=pitaya)
-    process.draw(n=100, N=100, style='Solarize_Light2', colormap="coolwarm", dpi=200)
-    process.draw(n=100, N=100, style='ggplot', dpi=200)
-    process.draw(n=100, N=100, style='ggplot', colormap="Spectral", dpi=200)
-    process.draw(n=100, N=100, style='ggplot', colormap="BrBG", dpi=200)
-    process.draw(n=100, N=100, style='bmh', dpi=200)
-    process.draw(n=100, N=100, style='dark_background', dpi=200)
-    process.plot(n=100, N=10)
+    cir.draw(n=100, N=200)
+    cev.draw(n=100, N=200)
