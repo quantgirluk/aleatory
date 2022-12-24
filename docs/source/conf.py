183dcc2eecfbd9f58c3303a403e3c242c92247ef
Quant Girl <quantgirl.uk@gmail.com>
2022-12-24 19:37:15 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ import importlib
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../..'))..
+sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..', '..', 'aleatory')))
 aleatory = importlib.import_module("aleatory")
 
