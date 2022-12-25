3c7a99c5a4417b9637bab4d641aabc0af7655a33
Quant Girl <quantgirl.uk@gmail.com>
2022-12-25 20:52:28 +0000

    Update test_transormation_em.py

--- tests/test_transormation_em.py
+++ tests/test_transormation_em.py
@@ -1,4 +1,6 @@
 from aleatory.processes import CIRProcess, CEVProcess
+
+
 def test_transformation_em():
     cir = CIRProcess(theta=1.0, mu=2.0, sigma=0.25)
     cev = CEVProcess(gamma=1.5, mu=0.50, sigma=0.1, initial=1.0)
