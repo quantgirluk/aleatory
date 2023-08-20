ef66e6713e86d9997acc4539fd76bac9f80fda28
Quant Girl <quantgirl.uk@gmail.com>
2023-08-20 21:19:37 +0100

    Updated test_charts.py

--- tests/test_charts.py
+++ tests/test_charts.py
@@ -55,7 +55,7 @@ def test_figures_examples():
     for process in processes:
 
         process.plot(n=100, N=5, figsize=(9.5, 6), dpi=200)
-        process.plot(n=100, N=5, title='My favourite figure', figsize=(9.5, 6), dpi=200)  # figure_with_title
+        # process.plot(n=100, N=5, title='My favourite figure', figsize=(9.5, 6), dpi=200)  # figure_with_title
 
         name = process.name.replace(" ", "_").lower()
         if SAVE:
@@ -64,16 +64,16 @@ def test_figures_examples():
             figure = process.draw(n=100, N=200, figsize=(12, 6), dpi=200)
             figure.savefig(SAVE_PATH + name + '_drawn.png')
 
-        process.draw(n=100, N=200, figsize=(12, 6), dpi=200)
+        # process.draw(n=100, N=200, figsize=(12, 6), dpi=200)
         process.draw(n=100, N=200, envelope=True, figsize=(12, 6), dpi=200)
-        process.draw(n=100, N=200, marginal=False, figsize=(9.5, 6), dpi=200)
-        process.draw(n=100, N=200, marginal=False, envelope=True, figsize=(9.5, 6), dpi=200)
-
-        process.draw(n=100, N=200, figsize=(12, 6), dpi=200, title='My favourite figure')
-        process.draw(n=100, N=200, envelope=True, figsize=(12, 6), dpi=200, title='My favourite figure')
-        process.draw(n=100, N=200, marginal=False, figsize=(9.5, 6), dpi=200, title='My favourite figure')
-        process.draw(n=100, N=200, marginal=False, envelope=True, figsize=(9.5, 6), dpi=200,
-                     title='My favourite figure')
+        # process.draw(n=100, N=200, marginal=False, figsize=(9.5, 6), dpi=200)
+        # process.draw(n=100, N=200, marginal=False, envelope=True, figsize=(9.5, 6), dpi=200)
+        #
+        # process.draw(n=100, N=200, figsize=(12, 6), dpi=200, title='My favourite figure')
+        # process.draw(n=100, N=200, envelope=True, figsize=(12, 6), dpi=200, title='My favourite figure')
+        # process.draw(n=100, N=200, marginal=False, figsize=(9.5, 6), dpi=200, title='My favourite figure')
+        # process.draw(n=100, N=200, marginal=False, envelope=True, figsize=(9.5, 6), dpi=200,
+        #              title='My favourite figure')
 
 
 def test_quick_start():
