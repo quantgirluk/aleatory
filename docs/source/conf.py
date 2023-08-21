6df6bbf81aeaaeb6462a206f6097a7e4068bfb83
Quant Girl <quantgirl.uk@gmail.com>
2023-08-21 23:00:51 +0100

    Updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2022, Dialid Santiago'
 author = 'Dialid Santiago'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 
