1183689cfdc6f2e0569efa179a7bd9abfa87ae97
Quant Girl <quantgirl.uk@gmail.com>
2023-11-03 22:25:59 +0000

    Update test_bes.py

--- tests/test_bes.py
+++ tests/test_bes.py
@@ -26,12 +26,5 @@ def test_non_integer(dim=3.5):
 
 def test_non_integer_besq(dim=4.5):
     p = BESQProcess(dim=dim)
-
-    # sam = p.sample(n=100)
-    # times = np.linspace(0, 1.0, 100)
-    # plt.plot(times,sam)
-    # plt.show()
-
-    # sim = p.simulate(n=10, N=10)
     p.draw(n=100, N=200)
     p.plot(n=100, N=5)
