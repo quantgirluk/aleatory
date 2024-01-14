9d359133f583017f8c5de438822b6337da790729
Quant Girl <quantgirl.uk@gmail.com>
2024-01-14 16:32:43 +0000

    Update __init__.py

--- aleatory/stats/__init__.py
+++ aleatory/stats/__init__.py
@@ -1,2 +1,20 @@
-from aleatory.stats.non_central_chi import ncx
+"""
+.. _statsref:
+
+==============================================
+Statistical functions (:mod:`aleatory.stats`)
+==============================================
+
+.. currentmodule:: aleatory.stats
+
+This module contains the implementation of the Non-Central Chi probability distribution.
+
+.. autosummary::
+   :toctree: non_central_chi/
+
+   ncx      -- Non-central chi
+
+"""
+
+from aleatory.stats.non_central_chi import ncx, ncx_gen
 
