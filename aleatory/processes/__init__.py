d20f54467d9c6bb68320c634b2d0cb0aa31c7bb3
Quant Girl <quantgirl.uk@gmail.com>
2024-05-19 18:29:35 +0100

    Update __init__.py

--- aleatory/processes/__init__.py
+++ aleatory/processes/__init__.py
@@ -1,7 +1,7 @@
 from aleatory.processes.euler_maruyama.vasicek import Vasicek
 from aleatory.processes.euler_maruyama.ornstein_uhlenbeck import OUProcess
 from aleatory.processes.euler_maruyama.cir_process import CIRProcess
-from aleatory.processes.euler_maruyama.cev_process import CEVProcess
+
 from aleatory.processes.analytical.geometric_brownian import GBM
 from aleatory.processes.analytical.brownian_motion import BrownianMotion
 from aleatory.processes.analytical.bes import BESProcess
@@ -9,3 +9,5 @@ from aleatory.processes.analytical.besq import BESQProcess
 from aleatory.processes.analytical.brownian_bridge import BrownianBridge
 from aleatory.processes.analytical.brownian_excursion import BrownianExcursion
 from aleatory.processes.analytical.brownian_meander import BrownianMeander
+from aleatory.processes.euler_maruyama.ckls_process import CKLSProcess
+from aleatory.processes.euler_maruyama.cev_process import CEVProcess
