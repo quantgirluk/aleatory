b405da5c8688bee8d18b6a924a826842536624bd
Quant Girl <quantgirl.uk@gmail.com>
2024-12-11 22:24:21 +0000

    Adding functionality to support the variance gamma distribution and process.

--- aleatory/processes/analytical/jump/variance_gamma.py
+++ aleatory/processes/analytical/jump/variance_gamma.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 from aleatory.processes.base_analytical import SPAnalytical
+from aleatory.processes.analytical.gaussian import GaussianIncrements
 from aleatory.utils.utils import (
     check_positive_integer,
     get_times,
 )
+from aleatory.stats.variance_gamma import vg
 
 
 class VarianceGammaProcess(SPAnalytical):
@@ -25,57 +27,23 @@ class VarianceGammaProcess(SPAnalytical):
         return f"VarianceGammaProcess(theta = {self.theta}, nu={self.nu}, sigma={self.sigma})"
 
     def _sample_variance_gamma_process(self, n):
-        """Generate a realization of a Variance gamma process."""
+        """Generate a realization of a Variance Gamma process."""
         check_positive_integer(n)
-
         dt = self.T / n
-        shape = dt / self.nu
-        scale = self.nu
-
-        gammas = self.rng.gamma(shape=shape, scale=scale, size=n - 1)
-        gaussian_increments = self.rng.normal(loc=0, scale=1, size=n - 1)
-
+        gammas = self.rng.gamma(shape=dt / self.nu, scale=self.nu, size=n - 1)
+        gaussian_increments = self.rng.normal(loc=0.0, scale=1.0, size=n - 1)
         increments = (
             self.theta * gammas + self.sigma * np.sqrt(gammas) * gaussian_increments
         )
         sample = np.cumsum(increments)
         sample = np.concatenate(([0], sample))
-
         return sample
-
-    def _sample_gamma_bridge_sampling(self, n):
-        """
-        Gamma Bridge Sampling for the Variance Gamma process.
-        """
-        pass
-        # dt = self.T / n
-        # times = self.times
-        #
-        # # Simulate terminal value of the process
-        # G_T = np.random.gamma(shape=self.T / self.nu, scale=self.nu)
-        # X_T = self.rng.normal(loc=self.theta * G_T, scale=(self.sigma**2) * G_T)
-        #
-        # # Initialize the Gamma process
-        # gamma_process = np.zeros(n)
-        # gamma_process[-1] = G_T
-        #
-        # # Bridge sampling
-        # for i in range(1, n - 1):
-        #     t_i = times[i]
-        #     mean = (t_i / self.T) * G_T
-        #     variance = (t_i * (self.T - t_i)) / self.T**2 * G_T / self.nu
-        #     shape = mean**2 / variance
-        #     scale = variance / mean
-        #     gamma_process[i] = np.random.gamma(shape=shape, scale=scale)
-
-        # return gamma_process
 
     def sample(self, n):
         """Generate a realization.
 
         :param int n: the number of increments to generate
         """
-
         self.n = n
         self.times = get_times(self.T, self.n)
         return self._sample_variance_gamma_process(n)
@@ -100,6 +68,14 @@ class VarianceGammaProcess(SPAnalytical):
         variances = self._process_variance(times=times)
         return variances
 
+    def get_marginal(self, t):
+
+        a = 2.0 * t / self.nu
+        b = 0.5 * self.theta * self.nu
+        c = self.sigma * np.sqrt(0.5 * self.nu)
+        marginal = vg(r=a, theta=b, sigma=c)
+        return marginal
+
     def plot(self, n, N, mode="steps", title=None, **fig_kw):
         """
         Simulates and plots paths/trajectories from the instanced stochastic process.
@@ -115,7 +91,15 @@ class VarianceGammaProcess(SPAnalytical):
         self._plot_process(n=n, N=N, mode=mode, title=title, **fig_kw)
 
     def draw(
-        self, n, N, marginal=True, envelope=False, mode="steps", title=None, **fig_kw
+        self,
+        n,
+        N,
+        mode="steps",
+        marginal=True,
+        envelope=False,
+        empirical_envelope=True,
+        title=None,
+        **fig_kw,
     ):
         """
         Simulates and plots paths/trajectories from the instanced stochastic process.
@@ -130,53 +114,69 @@ class VarianceGammaProcess(SPAnalytical):
         :param int N: number of paths to simulate
         :param bool marginal: defaults to True
         :param bool envelope: defaults to False
+        :param bool empirical_envelope : defaults to True
         :param mode: defines the type of plot to produce (e.g. "steps", "points" or "steps+points")
         :param str title: string optional default to the name of the process
         :return:
         """
         return self._draw_qqstyle(
-            n, N, marginal=marginal, mode=mode, envelope=envelope, title=title, **fig_kw
+            n,
+            N,
+            marginal=marginal,
+            mode=mode,
+            envelope=envelope,
+            title=title,
+            empirical_envelope=empirical_envelope,
+            estimate_quantiles=True,
+            **fig_kw,
         )
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+#
+#     import matplotlib.pyplot as plt
+#
+#     qs = "https://raw.githubusercontent.com/quantgirluk/matplotlib-stylesheets/main/quant-pastel-light.mplstyle"
+#     plt.style.use(qs)
+#
+#     for T in [5.0, 10.0, 20.0]:
+#         p = VarianceGammaProcess(T=T, theta=0.0, sigma=1.0)
+#
+#         for n_grid in [500]:
+#             p.draw(n=n_grid, N=200, figsize=(12, 7), style=qs, envelope=True)
+# p.draw(n=n_grid, N=200, figsize=(12, 7), style=qs, envelope=True)
 
-    import matplotlib.pyplot as plt
+# end_points = [path[-1] for path in p.paths]
+# plt.hist(end_points, density=True)
+# plt.show()
+# vals = p.marginal_density()
 
-    qs = "https://raw.githubusercontent.com/quantgirluk/matplotlib-stylesheets/main/quant-pastel-light.mplstyle"
-    plt.style.use(qs)
-
-    p = VarianceGammaProcess(T=10.0, theta=0.0, sigma=1.0)
-
-    for n_grid in [20, 200]:
-        p.draw(n=n_grid, N=100, figsize=(12, 7), style=qs, colormap="cividis")
-
-    #
-    # p.plot(n=10, N=5, figsize=(10, 6), style=qs)
-    # p.plot(
-    #     n=20,
-    #     N=5,
-    #     figsize=(10, 6),
-    #     style=qs,
-    #     mode="steps+points",
-    #     title="Variance Gamma Process Paths",
-    # )
-    # p.draw(
-    #     n=100,
-    #     N=100,
-    #     figsize=(12, 7),
-    #     style=qs,
-    #     colormap="viridis",
-    #     marginal=False,
-    #     mode="steps+points",
-    # )
-    # p.draw(n=100, N=100, figsize=(12, 7), style=qs, colormap="cool")
-    # p.plot(n=20, N=5, figsize=(10, 6), style=qs)
-    # p = VarianceGammaProcess(T=100)
-    # p.draw(n=100, N=100, figsize=(12, 7), style=qs, colormap="viridis")
-    # p = VarianceGammaProcess(theta=1.5, nu=0.5, sigma=2.0, T=20)
-    # p.draw(n=100, N=200, figsize=(12, 7), style=qs, colormap="twilight")
-    # p = VarianceGammaProcess(theta=0.0, nu=0.5, sigma=1.0, T=1.0)
-    # p.draw(n=100, N=200, figsize=(12, 7), style=qs, colormap="winter", envelope=True)
-    # p = VarianceGammaProcess(theta=-1.0, nu=4.0, sigma=2.0, T=100.0)
-    # p.draw(n=100, N=200, figsize=(12, 7), style=qs, colormap="summer", envelope=True)
+#
+# p.plot(n=10, N=5, figsize=(10, 6), style=qs)
+# p.plot(
+#     n=20,
+#     N=5,
+#     figsize=(10, 6),
+#     style=qs,
+#     mode="steps+points",
+#     title="Variance Gamma Process Paths",
+# )
+# p.draw(
+#     n=100,
+#     N=100,
+#     figsize=(12, 7),
+#     style=qs,
+#     colormap="viridis",
+#     marginal=False,
+#     mode="steps+points",
+# )
+# p.draw(n=100, N=100, figsize=(12, 7), style=qs, colormap="cool")
+# p.plot(n=20, N=5, figsize=(10, 6), style=qs)
+# p = VarianceGammaProcess(T=100)
+# p.draw(n=100, N=100, figsize=(12, 7), style=qs, colormap="viridis")
+# p = VarianceGammaProcess(theta=1.5, nu=0.5, sigma=2.0, T=20)
+# p.draw(n=100, N=200, figsize=(12, 7), style=qs, colormap="twilight")
+# p = VarianceGammaProcess(theta=0.0, nu=0.5, sigma=1.0, T=1.0)
+# p.draw(n=100, N=200, figsize=(12, 7), style=qs, colormap="winter", envelope=True)
+# p = VarianceGammaProcess(theta=-1.0, nu=4.0, sigma=2.0, T=100.0)
+# p.draw(n=100, N=200, figsize=(12, 7), style=qs, colormap="summer", envelope=True)
--- aleatory/stats/variance_gamma.py
+++ aleatory/stats/variance_gamma.py
@@ -1,127 +1,118 @@
-# from scipy.stats import rv_continuous, chi, ncx2, chi2
-# import numpy as np
-# from scipy.special import xlogy, ive, eval_genlaguerre
-#
-#
-# def _lazywhere(cond, arrays, f, fillvalue=None, f2=None):
-#     """
-#     np.where(cond, x, fillvalue) always evaluates x even where cond is False.
-#     This one only evaluates f(arr1[cond], arr2[cond], ...).
-#
-#     Examples
-#     --------
-#     # >>> a, b = np.array([1, 2, 3, 4]), np.array([5, 6, 7, 8])
-#     # >>> def f(a, b):
-#     # ...     return a*b
-#     # >>> _lazywhere(a > 2, (a, b), f, np.nan)
-#     array([ nan,  nan,  21.,  32.])
-#
-#     Notice, it assumes that all `arrays` are of the same shape, or can be
-#     broadcasted together.
-#
-#     """
-#     cond = np.asarray(cond)
-#     if fillvalue is None:
-#         if f2 is None:
-#             raise ValueError("One of (fillvalue, f2) must be given.")
-#         else:
-#             fillvalue = np.nan
-#     else:
-#         if f2 is not None:
-#             raise ValueError("Only one of (fillvalue, f2) can be given.")
-#
-#     args = np.broadcast_arrays(cond, *arrays)
-#     cond, arrays = args[0], args[1:]
-#     temp = tuple(np.extract(cond, arr) for arr in arrays)
-#     type_code = np.mintypecode([a.dtype.char for a in arrays])
-#     out = np.full(np.shape(arrays[0]), fill_value=fillvalue, dtype=type_code)
-#     np.place(out, cond, f(*temp))
-#     if f2 is not None:
-#         temp = tuple(np.extract(~cond, arr) for arr in arrays)
-#         np.place(out, ~cond, f2(*temp))
-#
-#     return out
-#
-#
-# def _ncx_log_pdf(x, df, nc):
-#     # We use (x**2 + nc**2)/2 = (x - nc)**2/2  + x*nc, and include the
-#     # factor of exp(-x*nc) into the ive function to improve numerical
-#     # stability at large values of x.
-#
-#     df2 = df / 2.0 - 1.0
-#     res = np.log(nc) + xlogy(df / 2.0, x / nc) - 0.5 * (x - nc) ** 2
-#     corr = ive(df2, x * nc)
-#     value = res + np.log(corr)
-#     return value
-#
-#
-# def _ncx_pdf(x, df, nc):
-#     df2 = df / 2.0 - 1.0
-#     res = np.log(nc) + xlogy(df / 2.0, x / nc) - 0.5 * (x - nc) ** 2
-#     corr = ive(df2, x * nc)
-#     value = np.exp(res) * corr
-#     return value
-#
-#
-# # noinspection PyMethodOverriding
-# class variance_gamma(rv_continuous):
-#     r"""A non-central chi continuous random variable.
-#
-#     %(before_notes)s
-#
-#     Notes
-#     -----
-#     The probability density function for `ncx` is:
-#
-#     .. math::
-#
-#         f(x, k, \lambda) =  \exp(-(x^2 + \lambda^2)/2)
-#             (x/\lambda)^{k/2}  \lambda I_{(k/2)-1}(\lambda x)
-#
-#     for :math:`x >= 0`, :math:`k > 0` and :math:`\lambda \ge 0`.
-#     :math:`k` specifies the degrees of freedom (denoted ``df`` in the
-#     implementation) and :math:`\lambda` is the non-centrality parameter
-#     (denoted ``nc`` in the implementation). :math:`I_\nu` denotes the
-#     modified Bessel function of first order of degree :math:`\nu`
-#     (`scipy.special.iv`).
-#
-#     `ncx` takes ``df`` and ``nc`` as shape parameters.
-#
-#     %(after_notes)s
-#
-#     %(example)s
-#     """
-#
-#     def _argcheck(self, df, nc):
-#         return (df > 0) & (nc >= 0)
-#
-#     def _rvs(self, df, nc, size=None, random_state=None):
-#         ncx2_samples = random_state.noncentral_chisquare(df, nc**2, size)
-#         return np.sqrt(ncx2_samples)
-#
-#     def _ppf(self, q, df, nc):
-#         cond = np.ones_like(q, dtype=bool) & (nc != 0)
-#         values = _lazywhere(cond, (q, df, nc**2), f=ncx2.ppf, f2=chi2.ppf)
-#         return np.sqrt(values)
-#
-#     def _logpdf(self, x, df, nc):
-#         cond = np.ones_like(x, dtype=bool) & (nc != 0)
-#         return _lazywhere(cond, (x, df, nc), f=_ncx_log_pdf, f2=chi.logpdf)
-#
-#     def _pdf(self, x, df, nc):
-#         cond = np.ones_like(x, dtype=bool) & (nc != 0)
-#         return _lazywhere(cond, (x, df, nc), f=_ncx_pdf, f2=chi.pdf)
-#
-#     def _stats(self, df, nc):
-#         alpha = (df / 2.0) - 1.0
-#         mu = np.sqrt(np.pi / 2.0) * eval_genlaguerre(0.5, alpha, -0.5 * nc**2)
-#         variance = df + nc**2 - mu**2
-#         m3 = 3.0 * np.sqrt(np.pi / 2.0) * eval_genlaguerre(1.5, alpha, -0.5 * nc**2)
-#         skewness = (m3 - 3.0 * mu * variance - mu**3) / (np.sqrt(variance) ** 3)
-#         m4 = (df + nc**2) ** 2 + 2.0 * (df + 2.0 * nc**2)
-#         kurtosis = m4 / (variance**2)
-#
-#         return (mu, variance, skewness, kurtosis)
-#
-#
-# ncx = ncx_gen(a=0.0, name="ncx")
+from scipy.stats import rv_continuous
+import numpy as np
+from scipy.special import kv, gamma
+
+
+def _vg_log_pdf(x, r, theta, sigma):
+
+    f1 = -np.log(sigma * np.sqrt(np.pi) * gamma(0.5 * r))
+    f2 = (theta * x) / sigma**2
+    f3 = (0.5 * (r - 1.0)) * np.log(np.abs(x) / (2.0 * np.sqrt(theta**2 + sigma**2)))
+    f4 = np.log(
+        kv(0.5 * (r - 1), (np.sqrt(theta**2 + sigma**2) * np.abs(x)) / sigma**2)
+    )
+
+    value = f1 + f2 + f3 + f4
+    return value
+
+
+def _vg_pdf(x, r, theta, sigma):
+    f1 = -np.log(sigma * np.sqrt(np.pi) * gamma(0.5 * r))
+    f2 = (theta * x) / sigma**2
+
+    # f30 = (np.abs(x) / (2.0 * np.sqrt(theta**2 + sigma**2))) ** (0.5 * (r - 1.0))
+
+    f3 = (0.5 * (r - 1.0)) * (
+        np.log(np.abs(x)) - np.log((2.0 * np.sqrt(theta**2 + sigma**2)))
+    )
+    special_part = kv(
+        0.5 * (r - 1.0), (np.sqrt(theta**2 + sigma**2) * np.abs(x)) / sigma**2
+    )
+    # f4 = np.log(special_part)
+    # value = np.exp(f1 + f2 + f3 + f4)
+    value = np.exp(f1 + f2 + f3) * special_part
+    return value
+
+
+# noinspection PyMethodOverriding
+class vg_gen(rv_continuous):
+    r"""A variance-gamma continuous random variable.
+
+    %(before_notes)s
+
+    Notes
+    -----
+    The probability density function for `vg` is:
+
+    .. math::
+
+        f(x, k, \lambda) =  \exp(-(x^2 + \lambda^2)/2)
+            (x/\lambda)^{k/2}  \lambda I_{(k/2)-1}(\lambda x)
+
+    for :math:`x >= 0`, :math:`k > 0` and :math:`\lambda \ge 0`.
+    :math:`k` specifies the degrees of freedom (denoted ``df`` in the
+    implementation) and :math:`\lambda` is the non-centrality parameter
+    (denoted ``nc`` in the implementation). :math:`I_\nu` denotes the
+    modified Bessel function of first order of degree :math:`\nu`
+    (`scipy.special.iv`).
+
+    `ncx` takes ``df`` and ``nc`` as shape parameters.
+
+    %(after_notes)s
+
+    %(example)s
+    """
+
+    def _argcheck(self, r, theta, sigma):
+        return (r > 0) & (sigma > 0)
+
+    def _rvs(self, r, theta, sigma, size=None, random_state=None):
+        s = random_state.gamma(shape=0.5 * r, scale=2.0, size=size)
+        z = random_state.normal(loc=0.0, scale=1.0, size=size)
+        vg_samples = theta * s + sigma * np.sqrt(s) * z
+        return vg_samples
+
+    def _logpdf(self, x, r, theta, sigma):
+        values = _vg_log_pdf(x, r, theta, sigma)
+        return values
+
+    def _pdf(self, x, r, theta, sigma):
+        values = _vg_pdf(x, r, theta, sigma)
+        return values
+
+    def _stats(self, r, theta, sigma):
+        mu = r * theta
+        mu2 = r * (sigma**2 + 2.0 * theta**2)
+        variance = mu2
+        mu3 = 2.0 * r * theta * (3.0 * sigma**2 + 4.0 * theta**2)
+        skewness = mu3 / (mu2 ** (3.0 / 2.0))
+        mu4 = (
+            3.0
+            * r
+            * (
+                (r + 2.0) * sigma**4
+                + (4.0 * r + 16.0) * ((theta * sigma) ** 2 + theta**4)
+            )
+        )
+        kurtosis = mu4 / (mu2**2)
+
+        return mu, variance, skewness, kurtosis
+
+
+vg = vg_gen(name="vg")
+
+
+if __name__ == "__main__":
+
+    import matplotlib.pyplot as plt
+
+    pars = {"r": 0.5, "theta": 1.0, "sigma": 1.0}
+    f = vg(r=pars["r"], theta=pars["theta"], sigma=pars["sigma"])
+    sample = f.rvs(size=500)
+    a = -2.0
+    b = 2.0
+    xs = np.linspace(a, b, 500)
+    ys = f.pdf(xs)
+    plt.hist(sample, density=True, bins=50)
+    plt.plot(xs, ys)
+    plt.show()
