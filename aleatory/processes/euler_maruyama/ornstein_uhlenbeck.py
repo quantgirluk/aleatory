40c8bc623ac20bc1cb8984fc7e2859c7932cad75
Quant Girl <quantgirl.uk@gmail.com>
2024-12-13 16:34:58 +0000

    Adding parameters to name + Black

--- aleatory/processes/euler_maruyama/ornstein_uhlenbeck.py
+++ aleatory/processes/euler_maruyama/ornstein_uhlenbeck.py
@@ -1,6 +1,7 @@
 """
 Ornstein-Uhlenbeck Process
 """
+
 from aleatory.processes.euler_maruyama.vasicek import Vasicek
 
 
@@ -37,12 +38,59 @@ class OUProcess(Vasicek):
     """
 
     def __init__(self, theta=1.0, sigma=0.5, initial=1.0, T=1.0, rng=None):
-        super().__init__(theta=theta, mu=0.0, sigma=sigma, initial=initial, T=T, rng=rng)
+        super().__init__(
+            theta=theta, mu=0.0, sigma=sigma, initial=initial, T=T, rng=rng
+        )
         self.n = 1.0
         self.dt = 1.0 * self.T / self.n
         self.times = None
-        self.name = "Ornstein–Uhlenbeck process"
+        self.name = f"Ornstein–Uhlenbeck process $X(\\theta={self.theta}, \\sigma={self.sigma})$ on $[0,{self.T}]$"
 
     def __str__(self):
         return "Ornstein–Uhlenbeck process with parameters {speed}, and {volatility} on [0, {T}].".format(
-            T=str(self.T), speed=str(self.theta), volatility=str(self.sigma))
+            T=str(self.T), speed=str(self.theta), volatility=str(self.sigma)
+        )
+
+
+# if __name__ == "__main__":
+#
+#     import matplotlib.pyplot as plt
+#
+#     qs = "https://raw.githubusercontent.com/quantgirluk/matplotlib-stylesheets/main/quant-pastel-light.mplstyle"
+#     plt.style.use(qs)
+#
+#     p = OUProcess()
+#     p.plot(n=200, N=5, figsize=(12, 7), style=qs)
+#
+#     for p, cm in [
+#         (OUProcess(), "twilight"),
+#         (
+#             OUProcess(
+#                 theta=5.0,
+#                 sigma=1.0,
+#                 initial=1.0,
+#                 T=4.0,
+#             ),
+#             "PiYG",
+#         ),
+#         (
+#             OUProcess(
+#                 theta=1.0,
+#                 sigma=1.0,
+#                 initial=5.0,
+#                 T=1.0,
+#             ),
+#             "viridis",
+#         ),
+#         (
+#             OUProcess(
+#                 theta=0.5,
+#                 sigma=2.0,
+#                 initial=3.0,
+#                 T=5.0,
+#             ),
+#             "Blues",
+#         ),
+#     ]:
+#         p.draw(n=400, N=200, figsize=(12, 7), style=qs, colormap=cm, envelope=True)
+
