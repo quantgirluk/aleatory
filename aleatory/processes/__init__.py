a60b6d3aa80f6f87bd085c3431c3e0837a6d5a31
Quant Girl <quantgirl.uk@gmail.com>
2025-01-06 14:38:20 -0600

    Adding new processes to __init__.py

--- aleatory/processes/__init__.py
+++ aleatory/processes/__init__.py
@@ -13,7 +13,17 @@ from aleatory.processes.analytical.brownian_meander import BrownianMeander
 from aleatory.processes.analytical.jump.random_walk import SimpleRandomWalk
 from aleatory.processes.analytical.jump.random_walk import RandomWalk
 from aleatory.processes.analytical.jump.gen_random_walk import GeneralRandomWalk
+from aleatory.processes.analytical.jump.galton_watson import GaltonWatson
+from aleatory.processes.analytical.jump.gamma import GammaProcess
 from aleatory.processes.analytical.jump.poisson import PoissonProcess
+from aleatory.processes.analytical.jump.poisson_nonhomogeneous import (
+    InhomogeneousPoissonProcess,
+)
+from aleatory.processes.analytical.jump.hawkes import HawkesProcess
+from aleatory.processes.analytical.jump.poisson_mixed import MixedPoissonProcess
+from aleatory.processes.analytical.jump.variance_gamma import VarianceGammaProcess
 
 from aleatory.processes.euler_maruyama.ckls_process import CKLSProcess
 from aleatory.processes.euler_maruyama.cev_process import CEVProcess
+
+from aleatory.processes.analytical.fractional_bm import fBM
