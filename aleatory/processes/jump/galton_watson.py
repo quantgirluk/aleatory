12eececddef925d85bfd30606a8227a681475263
Quant Girl <quantgirl.uk@gmail.com>
2025-01-08 21:59:01 -0600

    Docstrings updated

--- aleatory/processes/jump/galton_watson.py
+++ aleatory/processes/jump/galton_watson.py
@@ -1,3 +1,5 @@
+"""Galton-Watson process"""
+
 from aleatory.processes.base_analytical import SPAnalytical
 import numpy as np
 import pandas as pd
@@ -7,6 +9,32 @@ from matplotlib.gridspec import GridSpec
 
 
 class GaltonWatson(SPAnalytical):
+    r"""Galton-Watson process
+
+    .. image:: _static/galton_watson_draw.png
+
+
+    A Galton–Watson process is a stochastic process :math:`\{X_n : t \geq 0\}` which evolves
+    according to the recurrence formula:
+
+    .. math::
+
+        X_{0} = 1,
+
+    .. math::
+
+        X_{n+1} = \sum_{j=1}^{X_n} Z_j^{(n)},
+
+    where :math:`\{Z_j^{(n)}. : n, j \in \mathbb{N}\}` is  a set of independent and identically
+    distributed (i.i.d) natural number-valued random variables. In this case, we assume tha they
+    follow a Poisson distribution with parameter :math:`\mu>0`, i.e.:
+
+    .. math::
+        Z_j^{(n)} \sim Poi(\mu).
+
+    :parameter float mu: the parameter :math:`\mu>0` in the above definition
+
+    """
 
     def __init__(self, mu=1.0, rng=None):
         super().__init__(rng=rng, initial=1)
@@ -17,7 +45,9 @@ class GaltonWatson(SPAnalytical):
 
         self.generations = 0
         self.times = None
-        self.name = f"Galton Watson Process with Poisson Branching\n $Z_i\sim Poi(${self.mu}$)$"
+        self.name = (
+            f"Galton-Watson Process with Poisson Branching\n $Z_i\sim Poi(${self.mu}$)$"
+        )
 
     def _sample_galton_watson(self, generations=None):
         self.T = generations
@@ -27,7 +57,9 @@ class GaltonWatson(SPAnalytical):
         for i in range(generations):
             if current_population == 0:  # If the population is extinct, stop
                 break
-            next_population = sum(self.offspring_dist.rvs(1)[0] for _ in range(current_population))
+            next_population = sum(
+                self.offspring_dist.rvs(1)[0] for _ in range(current_population)
+            )
             populations.append(next_population)
             current_population = next_population
         return np.array(populations)
@@ -57,7 +89,9 @@ class GaltonWatson(SPAnalytical):
         """
         self.N = N
         self.T = generations
-        self.paths = [self._sample_galton_watson(generations=generations) for _ in range(N)]
+        self.paths = [
+            self._sample_galton_watson(generations=generations) for _ in range(N)
+        ]
         self.times = np.arange(generations + 1)
         return self.paths
 
@@ -82,7 +116,7 @@ class GaltonWatson(SPAnalytical):
         return self.paths
 
     def marginal_expectation(self, generations):
-        return (self.offspring_mean ** generations) * self.initial
+        return (self.offspring_mean**generations) * self.initial
 
     def marginal_variance(self, generations):
 
@@ -91,11 +125,24 @@ class GaltonWatson(SPAnalytical):
         if self.offspring_mean == 1:
             variances = sigma_squared * generations
         else:
-            variances = sigma_squared * mu ** (generations - 1.) * (1. - mu ** generations) / (1. - mu)
+            variances = (
+                sigma_squared
+                * mu ** (generations - 1.0)
+                * (1.0 - mu**generations)
+                / (1.0 - mu)
+            )
         return variances
 
-    def plot(self, N, generations=None, mode="steps",
-             style="seaborn-v0_8-whitegrid", color_survival=False, title=None, **fig_kw):
+    def plot(
+        self,
+        N,
+        generations=None,
+        mode="steps",
+        style="seaborn-v0_8-whitegrid",
+        color_survival=False,
+        title=None,
+        **fig_kw,
+    ):
         """
         Simulates and plots paths/trajectories from the instanced stochastic process. Simple plot of times
         versus process values as lines and/or markers.
@@ -107,25 +154,25 @@ class GaltonWatson(SPAnalytical):
 
         if color_survival:
             last_points = [path[-1] for path in paths]
-            colors = ['grey' if x < 1 else 'red' for x in last_points]
+            colors = ["grey" if x < 1 else "red" for x in last_points]
 
             with plt.style.context(style):
                 fig, ax = plt.subplots(**fig_kw)
-                for (path, color) in zip(paths, colors):
+                for path, color in zip(paths, colors):
                     times = np.arange(0, len(path))
-                    if mode == 'points':
+                    if mode == "points":
                         ax.scatter(times, path, s=7, color=color)
-                    elif mode == 'steps':
-                        ax.step(times, path, where='post', color=color)
-                    elif mode == 'linear':
+                    elif mode == "steps":
+                        ax.step(times, path, where="post", color=color)
+                    elif mode == "linear":
                         ax.plot(times, path)
-                    elif mode in ['points+steps', 'steps+points']:
-                        ax.step(times, path, where='post', alpha=0.5, color=color)
+                    elif mode in ["points+steps", "steps+points"]:
+                        ax.step(times, path, where="post", alpha=0.5, color=color)
                         color = plt.gca().lines[-1].get_color()
-                        ax.plot(times, path, 'o', color=color)
+                        ax.plot(times, path, "o", color=color)
                 ax.set_title(plot_title)
-                ax.set_xlabel('$t$')
-                ax.set_ylabel('$X(t)$')
+                ax.set_xlabel("$t$")
+                ax.set_ylabel("$X(t)$")
                 plt.show()
 
         else:
@@ -133,32 +180,43 @@ class GaltonWatson(SPAnalytical):
                 fig, ax = plt.subplots(**fig_kw)
                 for path in paths:
                     times = np.arange(0, len(path))
-                    if mode == 'points':
+                    if mode == "points":
                         ax.scatter(times, path, s=10)
-                    elif mode == 'steps':
-                        ax.step(times, path, where='post', linewidth=1.75)
-                    elif mode == 'linear':
+                    elif mode == "steps":
+                        ax.step(times, path, where="post", linewidth=1.75)
+                    elif mode == "linear":
                         ax.plot(times, path)
-                    elif mode in ['points+steps', 'steps+points']:
-                        ax.step(times, path, where='post', alpha=0.75)
+                    elif mode in ["points+steps", "steps+points"]:
+                        ax.step(times, path, where="post", alpha=0.75)
                         color = plt.gca().lines[-1].get_color()
-                        ax.plot(times, path, 'o', color=color)
+                        ax.plot(times, path, "o", color=color)
                     else:
-                        raise ValueError("mode can only take values 'points', 'steps', 'points+steps', or 'linear'.")
+                        raise ValueError(
+                            "mode can only take values 'points', 'steps', 'points+steps', or 'linear'."
+                        )
 
                 ax.set_xlim(right=generations + 1)
                 ax.set_title(plot_title)
-                ax.set_xlabel('$t$')
-                ax.set_ylabel('$X(t)$')
+                ax.set_xlabel("$t$")
+                ax.set_ylabel("$X(t)$")
                 plt.show()
 
         return fig
 
-    def draw(self, N, generations=None, style="seaborn-v0_8-whitegrid", colormap="RdYlBu_r", envelope=False,
-             marginal=True, colorspos=None, mode="linear", **fig_kw):
+    def draw(
+        self,
+        N,
+        generations=None,
+        style="seaborn-v0_8-whitegrid",
+        colormap="RdYlBu_r",
+        envelope=False,
+        marginal=True,
+        colorspos=None,
+        mode="steps",
+        **fig_kw,
+    ):
 
         title = self.name
-        # self.simulate_upto(N, generations=generations)
         self.simulate(N, generations=generations)
         paths = self.paths
         times = self.times
@@ -176,29 +234,42 @@ class GaltonWatson(SPAnalytical):
                 ax1 = fig.add_subplot(gs[:4])
                 ax2 = fig.add_subplot(gs[4:], sharey=ax1)
 
-                n, bins, patches = ax2.hist(last_points, n_bins, orientation='horizontal', density=False)
+                n, bins, patches = ax2.hist(
+                    last_points, n_bins, orientation="horizontal", density=False
+                )
                 for c, pat in zip(col, patches):
-                    plt.setp(pat, 'facecolor', cm(c))
-                my_bins = pd.cut(last_points, bins=bins, labels=range(len(bins) - 1), include_lowest=True)
+                    plt.setp(pat, "facecolor", cm(c))
+                my_bins = pd.cut(
+                    last_points,
+                    bins=bins,
+                    labels=range(len(bins) - 1),
+                    include_lowest=True,
+                )
                 colors = [col[b] for b in my_bins]
 
                 plt.setp(ax2.get_yticklabels(), visible=False)
-                ax2.set_title('$X_T$')
+                ax2.set_title("$X_T$")
 
                 for i in range(N):
-                    times_i = times[:len(paths[i])]
-                    if mode == 'points':
+                    times_i = times[: len(paths[i])]
+                    if mode == "points":
                         ax1.scatter(times_i, paths[i], color=cm(colors[i]), s=10)
-                    elif mode == 'steps':
-                        ax1.step(times_i, paths[i], color=cm(colors[i]), where='post')
-                    elif mode == 'linear':
+                    elif mode == "steps":
+                        ax1.step(times_i, paths[i], color=cm(colors[i]), where="post")
+                    elif mode == "linear":
                         ax1.plot(times_i, paths[i], color=cm(colors[i]))
-                    elif mode in ['points+steps', 'steps+points']:
-                        ax1.step(times_i, paths[i], color=cm(colors[i]), where='post', alpha=0.75)
+                    elif mode in ["points+steps", "steps+points"]:
+                        ax1.step(
+                            times_i,
+                            paths[i],
+                            color=cm(colors[i]),
+                            where="post",
+                            alpha=0.75,
+                        )
                         ax1.scatter(times_i, paths[i], color=cm(colors[i]), s=10)
 
                 if expectations is not None:
-                    ax1.plot(times, expectations, '--', lw=1.75, label='$E[X_t]$')
+                    ax1.plot(times, expectations, "--", lw=1.75, label="$E[X_t]$")
                     ax1.legend()
                 plt.subplots_adjust(wspace=0.2, hspace=0.5)
 
@@ -208,37 +279,56 @@ class GaltonWatson(SPAnalytical):
                     colors = [path[colorspos] / np.max(np.abs(path)) for path in paths]
                 else:
                     _, bins = np.histogram(last_points, n_bins)
-                    my_bins = pd.cut(last_points, bins=bins, labels=range(len(bins) - 1), include_lowest=True)
+                    my_bins = pd.cut(
+                        last_points,
+                        bins=bins,
+                        labels=range(len(bins) - 1),
+                        include_lowest=True,
+                    )
                     colors = [col[b] for b in my_bins]
 
                 for i in range(N):
-                    times_i = times[:len(paths[i])]
-                    if mode == 'points':
+                    times_i = times[: len(paths[i])]
+                    if mode == "points":
                         ax1.scatter(times_i, paths[i], color=cm(colors[i]), s=7)
-                    elif mode == 'steps':
-                        ax1.step(times_i, paths[i], color=cm(colors[i]), where='post')
-                    elif mode == 'linear':
-                        ax1.plot(times_i, paths[i], color=cm(colors[i]), )
-                    elif mode in ['points+steps', 'steps+points']:
-                        ax1.step(times_i, paths[i], color=cm(colors[i]), where='post', alpha=0.75)
+                    elif mode == "steps":
+                        ax1.step(times_i, paths[i], color=cm(colors[i]), where="post")
+                    elif mode == "linear":
+                        ax1.plot(
+                            times_i,
+                            paths[i],
+                            color=cm(colors[i]),
+                        )
+                    elif mode in ["points+steps", "steps+points"]:
+                        ax1.step(
+                            times_i,
+                            paths[i],
+                            color=cm(colors[i]),
+                            where="post",
+                            alpha=0.75,
+                        )
                         ax1.scatter(times_i, paths[i], color=cm(colors[i]))
 
                 if expectations is not None:
-                    ax1.plot(times, expectations, '--', lw=1.75, label='$E[X_t]$')
+                    ax1.plot(times, expectations, "--", lw=1.75, label="$E[X_t]$")
                     ax1.legend()
 
             fig.suptitle(title)
             ax1.set_xlim(right=generations + 1)
-            ax1.set_title(r'Simulated Paths $X_t, t \leq T$')  # Title
-            ax1.set_xlabel('$t$')
-            ax1.set_ylabel('$X(t)$')
+            ax1.set_title(r"Simulated Paths $X_t, t \leq T$")  # Title
+            ax1.set_xlabel("$t$")
+            ax1.set_ylabel("$X(t)$")
             plt.show()
 
         return fig
 
-# if __name__ == "__main__":
-#     qs = "https://raw.githubusercontent.com/quantgirluk/matplotlib-stylesheets/main/quant-pastel-light.mplstyle"
-#
+
+if __name__ == "__main__":
+    qs = "https://raw.githubusercontent.com/quantgirluk/matplotlib-stylesheets/main/quant-pastel-light.mplstyle"
+    p = GaltonWatson(mu=1.5)
+    p.plot(N=10, generations=10, style=qs, figsize=(12, 7))
+    p.draw(N=100, generations=10, style=qs, figsize=(12, 7), colormap="summer")
+
 #     for m in ["steps"]:
 #         # for par, gen in zip ([0.8, 1.0, 2.0], [25, 100, 10]):
 #         par = 1.0
