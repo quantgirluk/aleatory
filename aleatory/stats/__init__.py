e74b47f25009d081007eb909b5e92c71c66e0fa3
Quant Girl <quantgirl.uk@gmail.com>
2025-01-12 21:35:46 +0000

    Update4d doc strings, and imported vg distribution

--- aleatory/stats/__init__.py
+++ aleatory/stats/__init__.py
@@ -7,14 +7,17 @@ Statistical functions (:mod:`aleatory.stats`)
 
 .. currentmodule:: aleatory.stats
 
-This module contains the implementation of the Non-Central Chi probability distribution.
+This module contains the implementation of the following probability distributions:
+    -  Non-Central Chi
+    - Variance-Gamma
 
 .. autosummary::
    :toctree: non_central_chi/
 
    ncx      -- Non-central chi
+   vg       -- Variance-Gamma
 
 """
 
 from aleatory.stats.non_central_chi import ncx, ncx_gen
-
+from aleatory.stats.variance_gamma import vg
