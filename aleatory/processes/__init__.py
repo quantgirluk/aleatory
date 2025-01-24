e00df0f22a7c464f2cc64dd6b103eb8ec6d64cdb
Quant Girl <quantgirl.uk@gmail.com>
2025-01-24 13:35:59 +0000

    docstrings

--- aleatory/processes/__init__.py
+++ aleatory/processes/__init__.py
@@ -1,3 +1,38 @@
+"""
+
+.. currentmodule:: aleatory.processes
+
+The :py:mod:`aleatory.processes` module provides classes for following stochastic processes
+
+.. autosummary::
+   :toctree: processes
+
+   BrownianMotion               --  Brownian Motion
+   GBM                          --  Geometric Brownian Motion
+   BrownianBridge               --  Brownian Bridge
+   BrownianExcursion            --  Brownian Excursion
+   BrownianMeander              --  Brownian Meander
+   BESProcess                   -- Bessel Process
+   BESQProcess                  -- Squared Bessel Process
+   OUProcess                    -- Ornstein-Uhlenbeck Process
+   Vasicek                      --  Vasicek Process
+   CEVProcess                   --  Constant Elasticity Variance Process
+   CIRProcess                   -- Cox–Ingersoll–Ross (CIR) Process
+   CKLSProcess                  -- Chan-Karolyi-Longstaff-Sanders (CKLS) process
+   fBM                          -- fractional Brownian Motion
+   GaltonWatson                 -- Galton-Watson Process
+   GammaProcess                 -- Gamma Process
+   GeneralRandomWalk            -- General Random Walk Process
+   HawkesProcess                -- Hawkes Process
+   InverseGaussian              -- Inverse Gaussian Process
+   PoissonProcess               -- Poisson Process
+   MixedPoissonProcess          -- Mixed Poisson Process
+   InhomogeneousPoissonProcess -- Inhomogeneous Poisson Process
+   RandomWalk                   -- Random Walk Process
+   VarianceGammaProcess         -- Variance Gamma Process
+
+"""
+
 from aleatory.processes.analytical.brownian_motion import BrownianMotion
 from aleatory.processes.analytical.geometric_brownian import GBM
 
@@ -26,5 +61,5 @@ from aleatory.processes.jump.inverse_gaussian import InverseGaussian
 from aleatory.processes.jump.poisson import PoissonProcess
 from aleatory.processes.jump.poisson_mixed import MixedPoissonProcess
 from aleatory.processes.jump.poisson_nonhomogeneous import InhomogeneousPoissonProcess
-from aleatory.processes.jump.random_walk import RandomWalk
+from aleatory.processes.jump.random_walk import RandomWalk, SimpleRandomWalk
 from aleatory.processes.jump.variance_gamma import VarianceGammaProcess
