221af012855d08b2765555df6f375b5add6e6d7c
Quant Girl <quantgirl.uk@gmail.com>
2025-01-26 18:48:47 +0000

    version 1.0.1 style-sheets

--- examples/plot_brownian_bridge.py
+++ examples/plot_brownian_bridge.py
@@ -17,11 +17,8 @@ from aleatory.styles import qp_style
 qp_style()  # Use quant-pastel-style
 
 p = BrownianBridge()
-fig = p.draw(n=100, N=200, figsize=(10, 7), colormap="PiYG", envelope=True)
-fig.show()
-
+p.draw(n=200, N=100, figsize=(10, 7), colormap="Spectral", envelope=True)
 
 ###############################################################################
 
-fig = p.plot(n=100, N=10, figsize=(12, 7))
-fig.show()
+p.plot(n=100, N=10, figsize=(12, 7))
