9904ec17f794c60b996994d94e819412b0d63abe
Quant Girl <quantgirl.uk@gmail.com>
2025-04-05 21:50:55 +0100

    Update brownian_motion_2d.py
    Removing commented code

--- aleatory/processes/multi_dimensional/brownian_motion_2d.py
+++ aleatory/processes/multi_dimensional/brownian_motion_2d.py
@@ -7,11 +7,3 @@ class BM2D(CorrelatedBMs):
     def __init__(self, T=1):
         super().__init__(rho=0.0, T=T)
         self.name = "Brownian Motion 2D"
-
-
-# if __name__ == "__main__":
-#     p = BM2D()
-#
-#     f = p.plot_sample(n=100)
-#     g = p.plot_sample_coordinates(n=100)
-#     h = p.plot(n=100, N=3)
