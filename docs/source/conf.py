e21aa5d718b5c4f5c145422e2796a0f11728db26
Quant Girl <quantgirl.uk@gmail.com>
2026-05-29 12:34:48 +0100

    Bump version to 1.2.2 in configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2022-2026, Dialid Santiago"
 author = "Dialid Santiago"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.1"
+release = "1.2.2"
 
 # -- General configuration ---------------------------------------------------
 
